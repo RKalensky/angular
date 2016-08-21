056fd820989e1290fec439d8392c35d64016a5ab
RKalensky <roma.kalensky@gmail.com>
2016-08-21 16:01:23 +0300

    add(applicatio module): phones module

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-var phones = angular.module("phones", []);
-
-phones.controller("PhonesController", PhonesController);
-
-PhonesController.$inject = ["$http", "$scope"];
-function PhonesController($http, $scope) {
-    var me = this;
-    $http.get("src/phones.json")
-        .success(function (data) {
-            me.cardsList = data;
-        });
-    me.name = "zzz";
-    
-    setTimeout(function(me){
-        debugger;
-    }, 2000, $scope)
-}
+var phones = angular.module("phones", ["ngRoute"]);
